48ba4d06a58c7332c1be86102c0de1270e671d35
yeonsu <bestdustn@sookmyung.ac.kr>
2022-12-16 22:15:48 +0900

    [FEAT] 근접센서 스레드

--- BackgroundTask.py
+++ BackgroundTask.py
@@ -4,11 +4,20 @@ import time
 def alarm_start(c):
     c.acquire()
     print("알람 시작")
-    for i in range(5):
+    for i in range(10):
         print(f"thread_3 is running {i}")
-        time.sleep(0.1)
-    print("the third thread ends")
+        time.sleep(1)
+    print("alarm_start thread ends")
     c.notify()
     c.release()
 
 
+def activate_prox(c):
+    c.acquire()
+    print("근접센서 활성화 스레드 시작")
+    for i in range(5):
+        print(f"thread_3 is running {i}")
+        time.sleep(1)
+    print("activate_prox thread ends")
+    c.notify()
+    c.release()
